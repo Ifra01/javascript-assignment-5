0e3300adc3b6f7e67dd254cb211234c7c926b2f9
Ifra01 <jamalifra74@gmail.com>
2025-02-20 20:04:34 +0500

    created app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+// ASSIGNMENT - 5
+
+// CHAPTER - String Methods
+
+// Question 1 Write a program that takes two user inputs for first
+// and last name using prompt and merge them in a new variable titled fullName.
+// Greet the user using his full name. 
+// answer:
+// let firstName = prompt("enter your first name");
+// let lastName = prompt("enter your last name");
+// alert("hello " + firstName + " " + lastName);
+
+// Question 2 Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user input in your browser.
+// answer:
+// let userFavPhone = prompt("enter your favourite phone model");
+// document.write("your favourite phone model is " + userFavPhone + " and it's length is " + userFavPhone.length);
+
+// Question 3 Write a program to find the index of letter “n” in the word “Pakistani” and
+// display the result in your browser.
+// answer:
+// let nationality = "Pakistani";
+// document.write("nationality: " + nationality + "<br />" + "Length of 'n': " + nationality.indexOf('n'));
+
